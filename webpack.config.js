45dde333b5f5e5978321ecada0f0f544e6fcbc60
Cristiana Man <man.cristiana1@gmail.com>
2017-04-11 23:02:55 +0200

    Updated webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,30 @@ let config = {
             {
                 test: /\.less$/,
                 use: ['style-loader', 'css-loader?modules', 'less-loader']
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
             }
         ],
     },
