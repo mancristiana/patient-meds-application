4e479c38c77603c914e26fa306e4d5094b79f38e
Cristiana Man <man.cristiana1@gmail.com>
2017-04-14 21:08:10 +0200

    Added animated menu and routing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,17 +3,54 @@
  */
 
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import classNames from 'classnames/bind';
 import styles from './app.less';
 
 import {PatientStory} from './patient/patient-story.jsx';
 
 export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMenuOn: false
+        }
+    }
+
     render() {
+        let cx = classNames.bind(styles);
         return (
-            <div>
-                <div className={styles.app}>Patient Meds App</div>
-                <PatientStory />
-            </div>
+            <Router>
+                <div>
+                    <div className={cx({menuSection: true, on: this.state.isMenuOn})}>
+                        Patient Meds App
+                        <div className={cx({menuToggle: true, on: this.state.isMenuOn})}
+                             onClick={this._onMenuToggle.bind(this)}>
+                            <div className={styles.one}></div>
+                            <div className={styles.two}></div>
+                            <div className={styles.three}></div>
+                        </div>
+                        <nav onClick={this._onMenuToggle.bind(this)}>
+                            <ul role="navigation" className={cx({
+                                hidden: !this.state.isMenuOn
+                            })}>
+                                <li><Link to={'/'}>home</Link></li>
+                                <li><Link to={'/patients'}>patients</Link></li>
+                                <li><a href="">medicine</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <Route exact={true} path="/" render={() => (<h1>Welcome</h1>)} />
+                    <Route path="/patients" component={PatientStory}/>
+                </div>
+            </Router>
         )
+    }
+
+    _onMenuToggle() {
+        this.setState({
+            isMenuOn: !this.state.isMenuOn
+        });
     }
 }
